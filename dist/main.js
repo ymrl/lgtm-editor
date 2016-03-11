585d1d21359aad8f1503f9cf807ee8dbe38772de
ymrl <ymrl@c-fo.com>
2016-03-11 22:06:20 +0900

    update

--- dist/main.js
+++ dist/main.js
@@ -19748,6 +19748,10 @@
 
 	var _file_input2 = _interopRequireDefault(_file_input);
 
+	var _url_input = __webpack_require__(241);
+
+	var _url_input2 = _interopRequireDefault(_url_input);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19772,7 +19776,8 @@
 	        'div',
 	        null,
 	        _react2.default.createElement(_editor_preview2.default, { image: this.state.image }),
-	        _react2.default.createElement(_file_input2.default, null)
+	        _react2.default.createElement(_file_input2.default, null),
+	        _react2.default.createElement(_url_input2.default, null)
 	      );
 	    }
 	  }], [{
@@ -32684,6 +32689,97 @@
 	}(_react2.default.Component);
 
 	exports.default = FileInput;
+
+/***/ },
+/* 241 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _editor_actions = __webpack_require__(183);
+
+	var _editor_actions2 = _interopRequireDefault(_editor_actions);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var UrlInput = function (_React$Component) {
+	  _inherits(UrlInput, _React$Component);
+
+	  function UrlInput() {
+	    _classCallCheck(this, UrlInput);
+
+	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(UrlInput).call(this));
+
+	    _this.state = { url: '', gif: false };
+
+	    return _this;
+	  }
+
+	  _createClass(UrlInput, [{
+	    key: 'handleUrlInput',
+	    value: function handleUrlInput(e) {
+	      var url = e.target.value;
+	      this.setState({ url: url });
+	    }
+	  }, {
+	    key: 'handleCheckGif',
+	    value: function handleCheckGif(e) {
+	      var gif = e.target.checked;
+	      this.setState({ gif: gif });
+	    }
+	  }, {
+	    key: 'handleOk',
+	    value: function handleOk(e) {
+	      e.preventDefault();
+	      _editor_actions2.default.setImage({ url: this.state.url, fileType: this.state.gif ? 'image/gif' : null });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'form',
+	        null,
+	        _react2.default.createElement(
+	          'label',
+	          null,
+	          ' Image URL: ',
+	          _react2.default.createElement('input', { type: 'text', onChange: this.handleUrlInput.bind(this) })
+	        ),
+	        _react2.default.createElement(
+	          'label',
+	          null,
+	          _react2.default.createElement('input', { type: 'checkbox', onChange: this.handleCheckGif.bind(this) }),
+	          ' GIF'
+	        ),
+	        _react2.default.createElement(
+	          'button',
+	          { onClick: this.handleOk.bind(this) },
+	          'OK'
+	        )
+	      );
+	    }
+	  }]);
+
+	  return UrlInput;
+	}(_react2.default.Component);
+
+	exports.default = UrlInput;
 
 /***/ }
 /******/ ]);
