78f04f2d3b105aa56d0c19177af06ddd6de895e9
ymrl <ymrl@c-fo.com>
2016-03-21 16:19:24 +0900

    画像ペーストできるようになったぞ！！

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainContainer from './containers/main_container';
+import EditorActions from './actions/editor_actions';
 
 window.addEventListener('load', () => {
   ReactDOM.render(
     React.createElement(MainContainer),
     document.querySelector('#main')
   );
+  document.body.addEventListener('paste', (e) => {
+    for (let i = 0, l = e.clipboardData.items.length; i < l; i++) {
+      const item = e.clipboardData.items[i];
+      if(/^image\//.test(item.type)) {
+        const blob = item.getAsFile();
+        const reader = new FileReader();
+        reader.addEventListener('loadend', () => {
+          EditorActions.setImage({
+            url: reader.result,
+            fileType: item.type
+          })
+        })
+        reader.readAsDataURL(blob);
+      }
+    }
+  });
 });
